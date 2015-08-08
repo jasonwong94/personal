8f184ef2d8169b2cf6c815f43a7f5ff519e9ec8f
Jason W <jaw.wong@mail.utoronto.ca>
2015-08-08 02:11:33 -0400

    Basic functionality implemented

--- website.controller.js
+++ website.controller.js
@@ -4,8 +4,16 @@ app.controller( 'WebsiteController', WebsiteController );
 
 function WebsiteController(){ 
 	var viewInstance = this;
-	
+
+	viewInstance.Name = null;
+	viewInstance.enterUser = enterUser; 
+
 	console.log( "Controller succesfully created" );
 	console.debug(viewInstance);
 
+	function enterUser( name_ ){
+		console.debug( name_ );
+		viewInstance.Name = name_;
+		return;
+	}
 }
