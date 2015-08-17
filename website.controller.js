808bd08b1b05b8e7b80406e2127f3cc4028a56f2
Jason Wong <jasonwong2810@gmail.com>
2015-08-17 00:32:50 -0400

    Added function for user to skip form submission

--- website.controller.js
+++ website.controller.js
@@ -5,15 +5,38 @@ app.controller( 'WebsiteController', WebsiteController );
 function WebsiteController(){ 
 	var viewInstance = this;
 
+	// variables
 	viewInstance.Name = null;
-	viewInstance.enterUser = enterUser; 
+	viewInstance.VisitorType = VisitorType;
+	viewInstance.FormSubmitted = false;
+
+	// functions
+	viewInstance.submitUser = submitUser; 
+	viewInstance.skipSubmit = skipSubmit; 
+
 
 	console.log( "Controller succesfully created" );
 	console.debug(viewInstance);
 
-	function enterUser( name_ ){
-		console.debug( name_ );
+	function submitUser( name_ ){
+		console.debug( "Welcome " + name_ + "!");
 		viewInstance.Name = name_;
+		viewInstance.FormSubmitted = true;
+		return;
+	}
+
+	function skipSubmit(){
+		console.debug( "Bummer :(" );
+		viewInstance.Name = "Visitor";
+		viewInstance.FormSubmitted = true;
 		return;
 	}
 }
+
+var VisitorType = [
+	'Recruiter',
+	'Visitor',
+	'Professor',
+	'Friend',
+	'NyanCat'
+];
