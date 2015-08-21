fcf13ec2988463a9952db55dc1c519beefb670ef
Jason Wong <jaw.wong@mail.utoronto.ca>
2015-08-21 01:35:23 -0400

    Created a main css file that combines all the files together

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
 
+var numWatches = 0;
+
 gulp.task('compile-sass', function(){
+	numWatches++;
+
+	console.log('Build #: ' + numWatches );
 	gulp.src( './sass/*.scss')
 		.pipe( sass().on('error', sass.logError))
 		.pipe( gulp.dest('./css/'));
-
 } );
+
+gulp.task('default',function() {
+    gulp.watch('./sass/*.scss',['compile-sass']);
+});
